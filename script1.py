b3fc50ad64ebfed9fce7166984fa53e2cd7c267d
marvin <marvin23m@gmail.com>
2020-10-06 13:02:24 +0100

    Make program that converts kg to grams, pounds and ounces

--- script1.py
+++ script1.py
@@ -3,20 +3,30 @@ from tkinter import *
 window = Tk()
 
 
-def km_to_miles():
+def convert():
+    grams = float(e1_value.get()) * 1000
+    t1.insert(END, grams)
 
-    miles = float(e1_value.get()) * 1.6
-    t1.insert(END, miles)
+    pounds = float(e1_value.get()) * 2.20462
+    t2.insert(END, pounds)
 
+    ounces = float(e1_value.get()) * 35.274
+    t3.insert(END, ounces)
 
-b1 = Button(window, text='Execute', command=km_to_miles)
-b1.grid(row=0, column=0)
+b1 = Button(window, text='Convert', command=convert)
+b1.grid(row=0, column=3)
 
 e1_value = StringVar()
-e1 = Entry(window,textvariable=e1_value)
+e1 = Entry(window, textvariable=e1_value)
 e1.grid(row=0, column=1)
 
-t1 = Text(window, height=1, width=20)
-t1.grid(row=0, column=2)
+t1 = Text(window, height=1, width=10)
+t1.grid(row=1, column=1)
+
+t2 = Text(window, height=1, width=10)
+t2.grid(row=1, column=2)
+
+t3 = Text(window, height=1, width=10)
+t3.grid(row=1, column=3)
 
 window.mainloop()
